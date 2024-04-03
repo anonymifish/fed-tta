6d31af9e45e0f0606b9ca5d7df663cbc81aa826d
anonymifish <miller_yu@outlook.com>
2024-04-03 17:21:16 +0800

    add models

---
+++ src/model/lenet.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn.functional as F
+
+
+class LeNet28(torch.nn.Module):
+    def __init__(self):
+        super(LeNet28, self).__init__()
+        self.conv1 = torch.nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, stride=1, padding=2)
+        self.pool1 = torch.nn.MaxPool2d(kernel_size=2)
+        self.conv2 = torch.nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1)
+        self.pool2 = torch.nn.MaxPool2d(kernel_size=2)
+        self.fc1 = torch.nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = torch.nn.Linear(120, 84)
+        self.classifier = torch.nn.Linear(84, 10)
+
+    def forward(self, x):
+        out = F.relu(self.conv1(x))
+        out = self.pool1(out)
+        out = F.relu(self.conv2(out))
+        out = self.pool2(out)
+        out = out.view(-1, 16 * 5 * 5)
+        out = F.relu(self.fc1(out))
+        out = F.relu(self.fc2(out))
+        out = self.classifier(out)
+        return out
+
+
+class LeNet(torch.nn.Module):
+    def __init__(self, n_classes=10):
+        super(LeNet, self).__init__()
+        self.conv1 = torch.nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5, stride=1)
+        self.pool1 = torch.nn.MaxPool2d(kernel_size=2)
+        self.conv2 = torch.nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1)
+        self.pool2 = torch.nn.MaxPool2d(kernel_size=2)
+        self.fc1 = torch.nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = torch.nn.Linear(120, 84)
+        self.classifier = torch.nn.Linear(84, n_classes)
+
+    def forward(self, x):
+        out = F.relu(self.conv1(x))
+        out = self.pool1(out)
+        out = F.relu(self.conv2(out))
+        out = self.pool2(out)
+        out = out.view(-1, 16 * 5 * 5)
+        out = F.relu(self.fc1(out))
+        out = F.relu(self.fc2(out))
+        out = self.classifier(out)
+        return out
+
+
+def lenet(size, n_classes):
+    if size == 28:
+        return LeNet28()
+    elif size == 32:
+        return LeNet(n_classes)
+    else:
+        raise NotImplementedError('LeNet\'s input size should be 28 or 32')
